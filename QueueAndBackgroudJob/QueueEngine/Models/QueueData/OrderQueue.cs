103356a28bbc9fa4c2e48c2b1aaeb92a5cf4e251
Linh Pham <duylinh191@gmail.com>
2020-05-21 22:46:18 +0700

    update

---
+++ QueueAndBackgroudJob/QueueEngine/Models/QueueData/OrderQueue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueEngine.Models.QueueData
+{
+    public class OrderQueue
+    {
+    }
+}
---
+++ QueueAndBackgroudJob/QueueEngine/Models/QueueSetting/GoogleQueueSetting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueEngine.Models.QueueSetting
+{
+    public class GoogleQueueSetting
+    {
+        public string CredentialFile 
+    }
+}
